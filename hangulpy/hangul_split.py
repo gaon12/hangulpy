1862edf13c444dc02041d6895898e91ac1432522
gaon12 <gokirito12@gmail.com>
2024-05-29 20:56:35 +0900

    Fix Error #1

--- hangulpy/hangul_split.py
+++ hangulpy/hangul_split.py
@@ -15,14 +15,32 @@ def split_hangul_char(c):
         chosung_index = char_index // CHOSUNG_BASE
         jungsung_index = (char_index % CHOSUNG_BASE) // JUNGSUNG_BASE
         jongsung_index = char_index % JUNGSUNG_BASE
-        
+
         # 중성 및 종성 분해
         jungsung = JUNGSUNG_LIST[jungsung_index]
         jongsung = JONGSUNG_LIST[jongsung_index]
 
         jungsung_decomposed = JUNGSUNG_DECOMPOSE.get(jungsung, [jungsung])
         jongsung_decomposed = JONGSUNG_DECOMPOSE.get(jongsung, [jongsung])
-        
-        return [CHOSUNG_LIST[chosung_index]] + jungsung_decomposed + jongsung_decomposed
+
+        return [CHOSUNG_LIST[chosung_index]] + list(jungsung_decomposed) + list(jongsung_decomposed)
     
+    # 자모인 경우 추가 처리
+    if c in JUNGSUNG_DECOMPOSE:
+        return list(JUNGSUNG_DECOMPOSE[c])
+    if c in JONGSUNG_DECOMPOSE:
+        return list(JONGSUNG_DECOMPOSE[c])
+
     return [c]  # 한글이 아니면 그대로 반환
+
+def split_hangul_string(s):
+    """
+    주어진 문자열의 각 한글 음절을 초성, 중성, 종성으로 분해하여 배열 형태로 반환합니다.
+    
+    :param s: 문자열
+    :return: 각 한글 음절을 초성, 중성, 종성으로 분해한 결과를 포함하는 배열
+    """
+    result = []
+    for char in s:
+        result.extend(split_hangul_char(char))
+    return result
