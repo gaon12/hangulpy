96469d544be95a01643a17cc1c0ab0d7e7405371
gaon12 <gokirito12@gmail.com>
2024-12-23 17:02:30 +0900

    1.1.1 릴리즈 및 이슈 #7 #8 #9 #10 적용

--- hangulpy/hangul_ends_with_consonant.py
+++ hangulpy/hangul_ends_with_consonant.py
@@ -1,6 +1,6 @@
 # hangul_ends_with_consonant.py
 
-from hangulpy.utils import is_hangul_char, HANGUL_BEGIN_UNICODE, CHOSUNG_LIST, JONGSUNG_LIST
+from hangulpy.utils import is_hangul, HANGUL_BEGIN_UNICODE, CHOSUNG_LIST, JONGSUNG_LIST
 
 def ends_with_consonant(char):
 	"""
@@ -9,7 +9,7 @@ def ends_with_consonant(char):
 	:param char: 확인할 문자
 	:return: 자음으로 끝나면 True, 아니면 False
 	"""
-	if not is_hangul_char(char):
+	if not is_hangul(char):
 		return char in CHOSUNG_LIST
 
 	# 한글 음절의 유니코드 값을 기준으로 종성 인덱스를 확인합니다.
